825cef97219ac1f6e074c1fbad4ee37020de5f33
dhu0001 <16512386+dh0001@users.noreply.github.com>
2017-10-10 16:42:22 -0400

    less bugs, change readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # basic-calculator
+
+A C++ function which takes a string corresponding to an equation, and returns the value of the equation simplified to a single expression.  Division is integer division.
+
+An expression is a number.
+
+An operation is *, /, +, -.
+
+An equation can have brackets, numbers, and operations within.  It starts with an expression, and is followed by any number of (operation then expression).
+
+Brackets enclose an equation, and act as a single expression.
+
+Todo:
+- add verify function
+- float division
+- more operations
--- calculator.cc
+++ calculator.cc
@@ -21,34 +21,60 @@ int solve_no_space (const string &s) {
 	
 	ss.str(s);
 	
+	// check if it begins with bracket
 	if (ss.peek() == '(') ss.get();
 	
+	// get first number
 	int param1;
 	ss >> param1;
 	
-	if (ss.peek() == -1 or ss.peek() == ')') return param1;
+	if (ss.peek() == -1) return param1;	// check if end of file
 	
-	int param2;
+	
+	// get operator
 	char op;
-
 	ss >> op;
+
+	
+	// check if next parameter is a bracketed expression
+	bool in_bracket = false;
+	if (ss.peek() == '(') {
+		ss.get();	// skip the bracket
+		in_bracket = true;
+	}
+	
+	
+	// get the next paramater
+	int param2;
 	ss >> param2;
 	
-	string rest;
 	
+	// get the rest of the equation
+	string rest;
 	getline (ss, rest);
+	
 	
 	if (op == '+') {
 		return param1 + solve_no_space (to_string (param2) + rest);
-	}
+	} 
 	else if (op == '-') {
 		return param1 - solve_no_space (to_string (param2) + rest);
+	} 
+	else if (in_bracket){
+		
+		if (op == '*') {
+			return param1 * solve_no_space (to_string (param2) + rest);
+		}
+		else {
+			return param1 / solve_no_space (to_string (param2) + rest);
+		}
+		
 	}
 	else if (op == '*') {
 		return solve_no_space (to_string (param1 * param2) + rest);
 	}
 	else {
-		return solve_no_space (to_string (param1 / param2) + rest);
+		return solve_no_space (to_string ((float) param1 / (float) param2) + rest);
 	}
 }
 
